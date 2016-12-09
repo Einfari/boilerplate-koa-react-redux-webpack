68df47f37e30a4442beff7708c9466da5a024b8b
Niu <Niul@analytics.moodys.net>
2016-12-09 16:29:23 +0800

    first commit for init

---
+++ app/src/index.js
@@ -0,0 +1,9 @@
+// console.log('Wololo......');
+
+var component = require('./components/boilerplate.js');
+
+var app = document.createElement('div');
+
+document.body.appendChild(app);
+
+app.appendChild(component());
